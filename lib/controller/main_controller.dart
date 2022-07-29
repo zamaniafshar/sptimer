18f8d4a3033749aba4de8c1fe974cc6d93c4808a
MohammadAminZamaniAfshar <zamani3509@gmail.com>
2022-07-29 12:19:40 +0430

    implement init and sendAppToBackground functions

--- lib/controller/main_controller.dart
+++ lib/controller/main_controller.dart
@@ -1,18 +1,28 @@
 import 'package:get/get.dart';
 import 'package:pomotimer/controller/ui_controller.dart';
+import 'package:pomotimer/data/models/pomodoro_timer_model.dart';
 import 'package:pomotimer/data/pomodoro_timer/pomodoro_timer.dart';
+import 'package:pomotimer/data/services/foreground_service/timer_foreground_service.dart';
 
 class MainController {
   MainController() {
     init();
   }
   final UiController uiController = Get.find();
+  final TimerForegroundService service = TimerForegroundService();
 
-  void init() {
-    uiController.init(null);
+  void init() async {
+    PomodoroTimerModel? data;
+    if (await service.isStarted) {
+      data = await service.stop();
+    }
+    uiController.init(data);
   }
 
-  void sendAppToBackground() {}
-
-  void initData() {}
+  void sendAppToBackground() {
+    if (uiController.isStarted) {
+      service.start(uiController.data);
+      uiController.onPomodoroTimerFinish();
+    }
+  }
 }
