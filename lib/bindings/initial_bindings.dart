addb09f6eb82d6fb0700cd70d1a065000b15aa98
MohammadAminZamaniAfshar <zamani3509@gmail.com>
2022-08-06 21:27:02 +0430

    Init hive and create ForegroundServiceDB object

--- lib/bindings/initial_bindings.dart
+++ lib/bindings/initial_bindings.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:pomotimer/controller/ui_controller.dart';
+import 'package:pomotimer/data/database/foreground_service_db.dart';
 import 'package:pomotimer/data/services/foreground_service/timer_foreground_service.dart';
 import 'package:pomotimer/ui/screens/home/home_screen_controller.dart';
 import 'package:pomotimer/controller/main_controller.dart';
@@ -7,7 +10,12 @@ import 'package:pomotimer/ui/widgets/widgets.dart';
 
 class InitialBindings extends Bindings {
   @override
-  void dependencies() {
+  Future<void> dependencies() async {
+    await Hive.initFlutter();
+    ForegroundServiceDB foregroundServiceDB = ForegroundServiceDB();
+    await foregroundServiceDB.init();
+    print(foregroundServiceDB.isForegroundServiceStarted);
+    Get.put(foregroundServiceDB);
     Get.put(CountdownTimerController());
     Get.put(CustomSliderController());
     Get.put(HomeScreenController());
