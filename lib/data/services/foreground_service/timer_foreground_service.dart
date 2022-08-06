010502e6854b0504e83d1e14cd2e8b29f8af3ea0
MohammadAminZamaniAfshar <zamani3509@gmail.com>
2022-08-06 21:28:17 +0430

    Save state of foreground service

--- lib/data/services/foreground_service/timer_foreground_service.dart
+++ lib/data/services/foreground_service/timer_foreground_service.dart
@@ -1,19 +1,25 @@
 import 'dart:async';
 import 'package:android_long_task/android_long_task.dart';
+import 'package:get/get.dart';
+import 'package:pomotimer/data/database/foreground_service_db.dart';
 import 'package:pomotimer/data/models/pomodoro_timer_model.dart';
 import 'package:pomotimer/data/pomodoro_timer/pomodoro_timer.dart';
 import 'package:pomotimer/util/util.dart';
 
 class TimerForegroundService {
-  Future<bool> get isStarted async => await AppClient.getData() != null;
+  final ForegroundServiceDB _foregroundServiceDB = Get.find();
+
+  bool get isStarted => _foregroundServiceDB.isForegroundServiceStarted;
 
   Future<void> start(PomodoroTimerModel initialData) async {
+    await _foregroundServiceDB.saveState(true);
     await AppClient.execute(initialData);
   }
 
   Future<PomodoroTimerModel> stop() async {
     Map<String, dynamic>? map = await AppClient.getData();
     AppClient.stopService();
+    await _foregroundServiceDB.saveState(false);
     return PomodoroTimerModel.fromMap(map!);
   }
 }
