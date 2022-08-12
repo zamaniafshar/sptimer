731f9d599cb3e6ec7e98b7c034fdc418885a050f
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-12 19:02:04 +0430

    Add foreground service keys

--- lib/util/constants/constants.dart
+++ lib/util/constants/constants.dart
@@ -1,4 +1,10 @@
 const kRemainingDurationKey = 'RemainingDuration';
-const kMaxRoundKey = 'MaxRoundKey';
 const kPomodoroRoundKey = 'PomodoroRoundKey';
 const kIsWorkTimeKey = 'kIsWorkTimeKey';
+const kMaxRoundKey = 'kMaxRoundKey';
+
+const kStopServiceOrderKey = 'kStopServiceOrderKey';
+const kStartTimerKey = 'kStartPomotimerKey';
+const kStopTimerKey = 'kStopPomoTimerKey';
+const kCancelTimerKey = 'kCancelPomoTimerKey';
+const kGetDataOrderKey = 'kGetDataOrderKey';
