0c8bd0b9cbddc45814c82dd38315e02e8dd390f6
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-12 18:59:32 +0430

    Add pomodoro_service.dart file

---
+++ lib/data/services/foreground_service/pomodoro_service.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:pomotimer/data/pomodoro_timer/pomodoro_timer.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:pomotimer/util/util.dart';
+
+void onForegroundServiceStart(ServiceInstance service) async {
+  PomodoroTimer timer = PomodoroTimer();
+
+  service.on(kStartTimerKey).listen((event) {
+    timer.start(event![kMaxRoundKey]);
+    timer.listenEvery(const Duration(seconds: 1), () {
+      updateNotification(service, timer);
+    });
+  });
+  service.on(kStopServiceOrderKey).listen((event) {
+    timer.stop();
+  });
+  service.on(kCancelTimerKey).listen((event) {
+    timer.cancel();
+  });
+  service.on(kStopServiceOrderKey).listen((event) {
+    service.stopSelf();
+  });
+}
+
+void updateNotification(ServiceInstance service, PomodoroTimer timer) {
+  (service as AndroidServiceInstance).setForegroundNotificationInfo(
+    title: 'PomoTimer',
+    content: 'Remaining duration: ${timer.remainingDuration.toString().substring(3, 8)}',
+  );
+}
