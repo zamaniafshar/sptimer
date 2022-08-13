b236bf44f707d33b5de258f36e4d01165425eda5
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-13 12:55:11 +0430

    Add listener key

--- lib/util/constants/constants.dart
+++ lib/util/constants/constants.dart
@@ -2,10 +2,10 @@ const kRemainingDurationKey = 'RemainingDuration';
 const kPomodoroRoundKey = 'PomodoroRoundKey';
 const kIsWorkTimeKey = 'kIsWorkTimeKey';
 const kMaxRoundKey = 'kMaxRoundKey';
-const kMaxDurationKey = 'kMaxRoundKey';
+const kMaxDurationKey = 'kMaxDurationKey';
 
 const kStopServiceKey = 'kStopServiceOrderKey';
-
+const kListenerKey = 'kListenerKey';
 const kPauseTimerKey = 'kPauseTimerKey';
 const kStartTimerKey = 'kStartPomotimerKey';
 const kResumeTimerKey = 'kResumeTimerKey';
