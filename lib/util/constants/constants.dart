19f520d92668f59086d2949238dc4cd9f02b9207
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-16 19:03:20 +0430

    Export constants into constants.dart file

--- lib/util/constants/constants.dart
+++ lib/util/constants/constants.dart
@@ -1,15 +1,2 @@
-const kStopServiceKey = 'kStopServiceKey';
-const kNotifyStatusListenerKey = 'kNotifyStatusListenerKey';
-const kPauseTimerKey = 'kPauseTimerKey';
-const kStartTimerKey = 'kStartPomotimerKey';
-const kResumeTimerKey = 'kResumeTimerKey';
-const kCancelTimerKey = 'kCancelPomoTimerKey';
-
-const kGetPomodoroDataKey = 'kGetPomodoroDataKey';
-const kSendPomodoroDataKey = 'kSendPomodoroDataKey';
-const kGetIsTimerStartedKey = 'kGetIsTimerStartedKey';
-const kSendIsTimerStartedKey = 'kSendIsTimerStartedKey';
-
-const kPomodoroTimerStatusKey = 'kPomodoroTimerStatus';
-const kFinishedKey = 'kFinishKey';
-const kRestartedKey = 'kRestartedKey';
+export 'foreground_service_constant.dart';
+export 'ui_controller_constant.dart';
