117382963d7dcbe7e14e9dedc0db7b25d3b1b7e0
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-16 19:02:43 +0430

    Add foreground_service_constant.dart file

---
+++ lib/util/constants/foreground_service_constant.dart
@@ -0,0 +1,15 @@
+const kStopServiceKey = 'kStopServiceKey';
+const kNotifyStatusListenerKey = 'kNotifyStatusListenerKey';
+const kPauseTimerKey = 'kPauseTimerKey';
+const kStartTimerKey = 'kStartPomotimerKey';
+const kResumeTimerKey = 'kResumeTimerKey';
+const kCancelTimerKey = 'kCancelPomoTimerKey';
+
+const kGetPomodoroDataKey = 'kGetPomodoroDataKey';
+const kSendPomodoroDataKey = 'kSendPomodoroDataKey';
+const kGetIsTimerStartedKey = 'kGetIsTimerStartedKey';
+const kSendIsTimerStartedKey = 'kSendIsTimerStartedKey';
+
+const kPomodoroTimerStatusKey = 'kPomodoroTimerStatus';
+const kFinishedKey = 'kFinishKey';
+const kRestartedKey = 'kRestartedKey';
