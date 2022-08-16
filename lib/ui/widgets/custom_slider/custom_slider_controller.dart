3dcb61d80072c692b9e09574e290688bdf891a2e
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-16 19:18:36 +0430

    Add animationController to CustomSliderController

--- lib/ui/widgets/custom_slider/custom_slider_controller.dart
+++ lib/ui/widgets/custom_slider/custom_slider_controller.dart
@@ -1,19 +1,27 @@
+import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
 
-class CustomSliderController extends GetxController {
+class CustomSliderController extends GetxController with GetSingleTickerProviderStateMixin {
   final Rx<double> _sliderValue = 4.0.obs;
+  late final AnimationController _animationController;
   bool _active = true;
 
   double get sliderValue => _sliderValue.value;
 
-  set sliderValue(double newValue) {
+  void setSliderValue(double newValue) {
     newValue = newValue.roundToDouble();
-    if (newValue != _sliderValue.value && _active) {
+    if (_active) {
       _sliderValue.value = newValue;
     }
   }
 
-  void activate() => _active = true;
+  @override
+  void onInit() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(milliseconds: 250));
+    super.onInit();
+  }
 
+  void activate() => _active = true;
   void deactivate() => _active = false;
 }
