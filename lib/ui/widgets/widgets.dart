ed463c2ce8d5440ec12fe55713cae31615c46580
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-31 10:14:37 +0430

    Export new widgets

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -4,3 +4,4 @@ export 'circle_animated_button/circle_animated_button.dart';
 export 'circle_animated_button/circle_animated_button_controller.dart';
 export 'custom_slider/custom_slider.dart';
 export 'custom_slider/custom_slider_controller.dart';
+export 'background_container.dart';
