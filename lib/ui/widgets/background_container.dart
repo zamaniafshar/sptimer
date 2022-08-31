3b8ae0d37c77b92c16126c9ca0f92d2b001a5c2e
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-31 10:12:29 +0430

    Add background_container widget

---
+++ lib/ui/widgets/background_container.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BackgroundContainer extends StatelessWidget {
+  const BackgroundContainer({
+    Key? key,
+    this.height,
+    this.width,
+    this.padding,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+  final EdgeInsetsGeometry? padding;
+  final double? height;
+  final double? width;
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Material(
+        color: theme.colorScheme.surface,
+        elevation: 10,
+        borderRadius: BorderRadius.circular(15.r),
+        child: Padding(
+          padding: padding ?? EdgeInsets.symmetric(vertical: 20.h, horizontal: 20.w),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
