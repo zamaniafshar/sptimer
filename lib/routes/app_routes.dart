fc0eb983208bcf8ff15cd07a04daea2a63cd7770
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-08-31 10:10:41 +0430

    Add add_pomodoro_task_screen to routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 import 'package:pomotimer/routes/routes_name.dart';
+import 'package:pomotimer/ui/screens/add_pomodoro_task/add_pomodoro_task_screen.dart';
 import 'package:pomotimer/ui/screens/home/home_screen.dart';
 
 final appRoutes = [
   GetPage(
     name: RoutesName.homeScreen,
     page: () => const HomeScreen(),
+  ),
+  GetPage(
+    name: RoutesName.addPomodoroTaskScreen,
+    page: () => const AddPomodoroTaskScreen(),
   ),
 ];
--- lib/routes/routes_name.dart
+++ lib/routes/routes_name.dart
@@ -1,3 +1,4 @@
 class RoutesName {
   static const homeScreen = '/homeScreen';
+  static const addPomodoroTaskScreen = '/addPomodoroTaskScreen';
 }
