5f8c8bd462ea33b6fe4c925ca6d740baf4da4c81
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-11 18:09:59 +0430

    Add CircleNeumorphicButton

--- lib/ui/widgets/circle_neumorphic_button.dart
+++ lib/ui/widgets/circle_neumorphic_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pomotimer/theme/app_colors.dart';
 
 class CircleNeumorphicButton extends StatelessWidget {
   const CircleNeumorphicButton({
@@ -8,42 +9,34 @@ class CircleNeumorphicButton extends StatelessWidget {
     required this.color,
     required this.onTap,
     this.showInnerNeumorphicShape = false,
+    this.height,
+    this.width,
   }) : super(key: key);
 
   final Widget icon;
   final Color color;
+  final double? height;
+  final double? width;
   final bool showInnerNeumorphicShape;
   final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Neumorphic(
-      style: const NeumorphicStyle(
-        boxShape: NeumorphicBoxShape.circle(),
-        intensity: 1,
-        shadowDarkColor: Color(0x55000000),
-        depth: 10,
-        shape: NeumorphicShape.convex,
-      ),
-      child: Neumorphic(
-        style: showInnerNeumorphicShape
-            ? null
-            : const NeumorphicStyle(
-                boxShape: NeumorphicBoxShape.circle(),
-                intensity: 1,
-                depth: -20,
-                shape: NeumorphicShape.flat,
-                oppositeShadowLightSource: true,
-              ),
-        child: MaterialButton(
-          shape: const CircleBorder(),
-          color: color,
-          height: 100.w,
-          minWidth: 100.h,
-          onPressed: onTap,
-          child: icon,
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xff3ada9a),
+            Color(0xff00a355),
+          ],
         ),
       ),
+      child: icon,
     );
   }
 }
