242c7ea2200a19d6c3a24a67f6b0d34bb2945edd
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-17 18:28:03 +0430

    Fix name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pomotimer/bindings/initial_bindings.dart';
-import 'package:pomotimer/pomo_timer_app.dart';
+import 'package:pomotimer/pomotimer_app.dart';
 
 void main() async {
   await InitialBindings().dependencies();
