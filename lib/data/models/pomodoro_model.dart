3d024f62075f099800528d8e1591de43261d9bfa
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-17 12:41:37 +0430

    Refactor PomodoroModel :  it's not immutable any more Also i add title and currentMaxDuration getter

--- lib/data/models/pomodoro_model.dart
+++ lib/data/models/pomodoro_model.dart
@@ -1,55 +1,76 @@
 import 'package:get/get.dart';
-import 'package:pomotimer/data/enum/pomodoro_state.dart';
+import 'package:pomotimer/data/enum/pomodoro_status.dart';
+import 'package:pomotimer/data/enum/timer_status.dart';
 
 class PomodoroModelFields {
-  static const kRemainingDurationKey = 'RemainingDuration';
+  static const kCurrentRemainingDurationKey = 'kCurrentRemainingDurationKey';
   static const kPomodoroRoundKey = 'PomodoroRoundKey';
-  static const kPomodoroState = 'kPomodoroState';
+  static const kPomodoroStatus = 'kPomodoroStatus';
   static const kMaxRoundKey = 'kMaxRoundKey';
-  static const kMaxDurationKey = 'kMaxDurationKey';
   static const kWorkDurationKey = 'kWorkDurationKey';
   static const kShortBreakDurationKey = 'kShortBreakDurationKey';
   static const kLongBreakDurationKey = 'kLongBreakDurationKey';
+  static const kTimerStatus = 'kTimerStatus';
+  static const kTitle = 'kTitle';
 }
 
 class PomodoroModel {
-  PomodoroModel(
-      {required this.currentRemainingDuration,
-      required this.currentMaxDuration,
-      required this.workDuration,
-      required this.shortBreakDuration,
-      required this.longBreakDuration,
-      required this.maxPomodoroRound,
-      required this.pomodoroRound,
-      required this.pomodoroState});
-  final Duration currentRemainingDuration;
-  final Duration currentMaxDuration;
-  final Duration workDuration;
-  final Duration shortBreakDuration;
-  final Duration longBreakDuration;
-  final int? maxPomodoroRound;
-  final int pomodoroRound;
-  final PomodoroState pomodoroState;
+  PomodoroModel({
+    required this.title,
+    required this.workDuration,
+    required this.shortBreakDuration,
+    required this.longBreakDuration,
+    required this.maxPomodoroRound,
+    this.pomodoroRound = 1,
+    this.pomodoroStatus = PomodoroStatus.work,
+    this.timerStatus = TimerStatus.cancel,
+    Duration? currentRemainingDuration,
+  }) {
+    this.currentRemainingDuration = currentRemainingDuration ?? currentMaxDuration;
+  }
+
+  late Duration currentRemainingDuration;
+  String title;
+  Duration workDuration;
+  Duration shortBreakDuration;
+  Duration longBreakDuration;
+  int maxPomodoroRound;
+  int pomodoroRound;
+  PomodoroStatus pomodoroStatus;
+  TimerStatus timerStatus;
+
+  Duration get currentMaxDuration {
+    if (pomodoroStatus.isWorkTime) {
+      return workDuration;
+    } else if (pomodoroStatus.isShortBreakTime) {
+      return shortBreakDuration;
+    } else {
+      return longBreakDuration;
+    }
+  }
 
   Map<String, dynamic> toMap() => {
-        PomodoroModelFields.kMaxDurationKey: currentMaxDuration.inSeconds,
-        PomodoroModelFields.kRemainingDurationKey: currentRemainingDuration.inSeconds,
+        PomodoroModelFields.kCurrentRemainingDurationKey: currentRemainingDuration.inSeconds,
         PomodoroModelFields.kWorkDurationKey: workDuration.inSeconds,
         PomodoroModelFields.kShortBreakDurationKey: shortBreakDuration.inSeconds,
         PomodoroModelFields.kLongBreakDurationKey: longBreakDuration.inSeconds,
         PomodoroModelFields.kPomodoroRoundKey: pomodoroRound,
         PomodoroModelFields.kMaxRoundKey: maxPomodoroRound,
-        PomodoroModelFields.kPomodoroState: pomodoroState,
+        PomodoroModelFields.kPomodoroStatus: pomodoroStatus,
+        PomodoroModelFields.kTimerStatus: timerStatus,
+        PomodoroModelFields.kTitle: title,
       };
 
   static PomodoroModel fromMap(Map<String, dynamic> data) => PomodoroModel(
-        currentMaxDuration: (data[PomodoroModelFields.kMaxDurationKey] as int).seconds,
-        currentRemainingDuration: (data[PomodoroModelFields.kRemainingDurationKey] as int).seconds,
+        currentRemainingDuration:
+            (data[PomodoroModelFields.kCurrentRemainingDurationKey] as int).seconds,
         workDuration: (data[PomodoroModelFields.kWorkDurationKey] as int).seconds,
         shortBreakDuration: (data[PomodoroModelFields.kShortBreakDurationKey] as int).seconds,
         longBreakDuration: (data[PomodoroModelFields.kLongBreakDurationKey] as int).seconds,
         pomodoroRound: data[PomodoroModelFields.kPomodoroRoundKey] as int,
-        maxPomodoroRound: data[PomodoroModelFields.kMaxRoundKey] as int?,
-        pomodoroState: data[PomodoroModelFields.kPomodoroState] as PomodoroState,
+        maxPomodoroRound: data[PomodoroModelFields.kMaxRoundKey]! as int,
+        pomodoroStatus: data[PomodoroModelFields.kPomodoroStatus] as PomodoroStatus,
+        timerStatus: data[PomodoroModelFields.kTimerStatus] as TimerStatus,
+        title: data[PomodoroModelFields.kTitle] as String,
       );
 }
