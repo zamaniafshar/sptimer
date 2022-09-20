97019f32a0693506d927f387e37d7f6c2052fee5
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-20 11:59:57 +0430

    Fix imports in StartPomodoroTaskScreenBinding

--- lib/bindings/start_pomodoro_task_screen_binding.dart
+++ lib/bindings/start_pomodoro_task_screen_binding.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 import 'package:pomotimer/ui/screens/start_pomodoro_task_screen/start_pomodoro_task_screen_controller.dart';
-import 'package:pomotimer/ui/widgets/circle_animated_button/circle_animated_button_controller.dart';
-import 'package:pomotimer/ui/widgets/countdown_timer/countdown_timer_controller.dart';
+import 'package:pomotimer/ui/screens/start_pomodoro_task_screen/widgets/circle_animated_button/circle_animated_button_controller.dart';
+import 'package:pomotimer/ui/screens/start_pomodoro_task_screen/widgets/countdown_timer/countdown_timer_controller.dart';
 import 'package:pomotimer/ui/widgets/custom_slider/custom_slider_controller.dart';
 
 class StartPomodoroTaskScreenBinding implements Bindings {
