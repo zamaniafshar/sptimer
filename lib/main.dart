2877d0e6499c2f0fc1fae8f1ed97defff58c857e
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-22 09:52:40 +0330

    Fix error :

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:pomotimer/bindings/initial_bindings.dart';
 import 'package:pomotimer/pomotimer_app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await InitialBindings().dependencies();
   runApp(const PomoTimerApp());
 }
