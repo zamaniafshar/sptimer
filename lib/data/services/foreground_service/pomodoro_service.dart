da682d9296fd062b66986abc9dee9c0f6fac01e9
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-25 11:46:55 +0330

    Refactor pomodoro_service.dart

--- lib/data/services/foreground_service/pomodoro_service.dart
+++ lib/data/services/foreground_service/pomodoro_service.dart
@@ -1,23 +1,25 @@
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:pomotimer/data/models/pomodoro_model.dart';
+import 'package:pomotimer/data/models/pomodoro_task_model.dart';
 import 'package:pomotimer/data/pomodoro_timer/pomodoro_timer.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:pomotimer/util/util.dart';
 
 void onForegroundServiceStart(ServiceInstance service) async {
   service.invoke('started');
-  Map<String, dynamic>? initData = await service.on('initData').first;
+  Map<String, dynamic>? initState = await service.on('initData').first;
 
   PomodoroTimer timer = PomodoroTimer(
-    initState: PomodoroTaskModel.fromMap(initData!),
-  );
+      initState: PomodoroTaskModel.fromMap(initState!),
+      onFinish: (_) async {
+        service.stopSelf();
+      });
   timer.start();
   timer.listen(() {
-    updatePomodoroNotification(service, timer.remainingDuration);
+    updatePomodoroNotification(service, timer.state.currentRemainingDuration!);
   });
 
   service.on('getData').listen((event) {
-    service.invoke('sendData', getData(timer).toMap());
+    service.invoke('sendData', timer.state.toMap());
   });
 
   service.on(kStopServiceKey).listen((event) {
@@ -31,11 +33,3 @@ void updatePomodoroNotification(ServiceInstance service, Duration remainingDurat
     content: remainingDuration.toString().substring(2, 7),
   );
 }
-
-PomodoroTaskModel getData(PomodoroTimer timer) => PomodoroTaskModel(
-      currentMaxDuration: timer.maxDuration,
-      maxPomodoroRound: timer.maxPomodoroRound,
-      currentRemainingDuration: timer.remainingDuration,
-      pomodoroRound: timer.pomodoroRound,
-      isWorkTime: timer.isWorkTime,
-    );
