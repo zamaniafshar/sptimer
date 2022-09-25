a1ea5a9f4a5109eccb4535b4690656b70b30e41a
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-25 11:43:34 +0330

    Call initInitialDependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:pomotimer/bindings/initial_bindings.dart';
+import 'package:pomotimer/dependencies/initial_dependencies.dart';
 import 'package:pomotimer/pomotimer_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await InitialBindings().dependencies();
+  await initInitialDependencies();
   runApp(const PomoTimerApp());
 }
