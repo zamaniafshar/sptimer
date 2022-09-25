9c4d412ddadd88023d03921a10b07efed7595af9
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-25 11:47:58 +0330

    Implement foreground service logic when timer is started

--- lib/controller/main_controller.dart
+++ lib/controller/main_controller.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:pomotimer/routes/routes_name.dart';
+import 'package:pomotimer/ui/screens/start_pomodoro_task_screen/start_pomodoro_task_screen.dart';
 import 'package:pomotimer/ui/screens/start_pomodoro_task_screen/start_pomodoro_task_screen_controller.dart';
 import 'package:pomotimer/data/models/pomodoro_task_model.dart';
 import 'package:pomotimer/data/pomodoro_timer/pomodoro_timer.dart';
@@ -6,34 +8,25 @@ import 'package:pomotimer/data/services/foreground_service/timer_foreground_serv
 import 'package:pomotimer/util/constants/constants.dart';
 
 class MainController {
-  MainController() {
-    init();
-  }
-  final StartPomodoroTaskScreenController uiController = Get.find();
-  final TimerForegroundService service = TimerForegroundService();
-  late PomodoroTimer _timer;
+  late String initialRoute = RoutesName.baseScreen;
+
+  final TimerForegroundService _service = TimerForegroundService();
 
   Future<void> init() async {
-    // print('init ');
-    // await service.init();
-    // print('service init');
-    // PomodoroTimerModel data;
-    // if (await service.isRunning) {
-    //   print('stop service');
-    //   data = await service.stopService();
-    // }else{
-
-    // }
-    // print(data?.remainingDuration);
-    // _timer = PomodoroTimer(
-    //   initData: data,
-    //   onRestartTimer: onPomodoroTimerRestart,
-    //   onFinish: onPomodoroTimerFinish,
-    // );
-    // uiController.init(data);
+    await _service.init();
+    if (await _service.isRunning) {
+      initialRoute += RoutesName.startPomodoroTaskScreen;
+      PomodoroTaskModel state = await _service.stopService();
+      final controller = Get.put(StartPomodoroTaskScreenController());
+      await controller.init(state);
+    }
   }
 
   Future<void> onAppPaused() async {
-    // if (uiController.isTimerStarted) await service.startService(uiController.data);
+    try {
+      StartPomodoroTaskScreenController controller = Get.find();
+      await _service.startService(controller.state);
+      Get.back();
+    } catch (e) {}
   }
 }
