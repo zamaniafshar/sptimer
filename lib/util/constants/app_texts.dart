3d78ce938f164a50ae1eed11cd42a3b9c3cf3ee9
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-10-17 20:32:56 +0330

    Add kPomodoroFinishText to app texts

--- lib/util/constants/app_texts.dart
+++ lib/util/constants/app_texts.dart
@@ -1,3 +1,5 @@
 const kWorkTimeText = 'Stay focus for ';
 const kShortBreakText = 'Take a Short break for ';
 const kLongBreakText = 'Take a Long break for ';
+
+const kPomodoroFinishText = 'Good job! your pomodoro task has been completed.';
