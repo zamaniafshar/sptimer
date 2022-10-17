0da7a3ec3a61ba334acf4ded72ff6161ad3be50e
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-10-17 20:31:19 +0330

    Add back alert dialog

---
+++ lib/ui/screens/start_pomodoro_task_screen/widgets/back_alert_dialog.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void showBackAlertDialog(
+  BuildContext context, {
+  VoidCallback? onCancel,
+  VoidCallback? onContinue,
+}) {
+  final theme = Theme.of(context);
+  showDialog(
+    context: context,
+    builder: (_) => Center(
+      child: Container(
+        height: 200.h,
+        width: 300.w,
+        padding: EdgeInsets.all(20.r),
+        decoration: BoxDecoration(
+          color: theme.colorScheme.surface,
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Do you want to cancel the timer?',
+              style: theme.primaryTextTheme.headlineSmall,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    onCancel?.call();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.r),
+                    ),
+                  ),
+                  child: Text(
+                    'Cancel',
+                    style: theme.textTheme.titleLarge!.copyWith(color: Colors.white),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    onContinue?.call();
+                  },
+                  child: Text(
+                    'Continue',
+                    style: theme.primaryTextTheme.titleLarge!,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
