cfe08be277ac9e7fab7a56d7aedd357998ba9b8e
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-10-29 21:56:15 +0330

    Add some text to app texts

--- lib/util/constants/app_texts.dart
+++ lib/util/constants/app_texts.dart
@@ -4,3 +4,5 @@ const kLongBreakText = 'Take a Long break for ';
 
 const kPomodoroFinishText = 'Good job! your pomodoro task has been completed.';
 const kBackAlertDialog = 'Do you want to cancel the timer?';
+const kMuteAlertSnackbarText = '$kMutedText To hear timer sounds pls change the sounds settings';
+const kMutedText = 'Sound settings sets to mute.';
