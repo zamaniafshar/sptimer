b7595d1223a0470258a6be75672621df42e01b5f
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-12-08 12:39:57 +0330

    Add getAllReportageInDay and update function

--- lib/data/databases/tasks_reportage_database.dart
+++ lib/data/databases/tasks_reportage_database.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pomotimer/data/models/pomodoro_task_model.dart';
 import 'package:pomotimer/data/models/pomodoro_task_reportage_model.dart';
+import 'package:pomotimer/utils/utils.dart';
 
 class TasksReportageDatabase {
   late LazyBox _tasksBox;
@@ -24,6 +26,27 @@ class TasksReportageDatabase {
         final Map<dynamic, dynamic> data = await _tasksBox.get(key);
         final task = PomodoroTaskReportageModel.fromMap(data);
         result.add(task.copyWith(id: key));
+      }
+      return Right(result);
+    } catch (e) {
+      return Left(Exception(e.toString()));
+    }
+  }
+
+  Future<Either<Exception, List<PomodoroTaskReportageModel>>> getAllReportagesInDate(
+      DateTime date) async {
+    try {
+      final List<PomodoroTaskReportageModel> result = [];
+      for (int i = _tasksBox.keys.length - 1; i >= 0; i--) {
+        final key = _tasksBox.keys.elementAt(i);
+        final Map<dynamic, dynamic> data = await _tasksBox.get(key);
+        PomodoroTaskReportageModel task = PomodoroTaskReportageModel.fromMap(data);
+        task = task.copyWith(id: key);
+        if (task.startDate.isInSameDay(date)) {
+          result.add(task);
+        } else if (task.startDate.isBefore(date)) {
+          break;
+        }
       }
       return Right(result);
     } catch (e) {
@@ -70,10 +93,34 @@ class TasksReportageDatabase {
     }
   }
 
+  Future<Either<Exception, void>> update(PomodoroTaskModel pomodoroTaskModel) async {
+    try {
+      for (var key in _tasksBox.keys) {
+        final map = await _tasksBox.get(key);
+        final PomodoroTaskReportageModel reportageTask = PomodoroTaskReportageModel.fromMap(map);
+        if (reportageTask.pomodoroTaskId == pomodoroTaskModel.id!) {
+          _tasksBox.put(
+            key,
+            reportageTask
+                .copyWith(
+                  taskName: pomodoroTaskModel.title,
+                )
+                .toMap(),
+          );
+        }
+      }
+
+      return const Right(null);
+    } catch (e) {
+      return Left(Exception(e.toString()));
+    }
+  }
+
   Future<Either<Exception, void>> delete(int pomodoroTaskId) async {
     try {
-      for (var key in _tasksBox.keys.toList().reversed) {
-        final PomodoroTaskReportageModel task = await _tasksBox.get(key);
+      for (var key in _tasksBox.keys) {
+        final map = await _tasksBox.get(key);
+        final PomodoroTaskReportageModel task = PomodoroTaskReportageModel.fromMap(map);
         if (task.pomodoroTaskId == pomodoroTaskId) {
           _tasksBox.delete(key);
         }
