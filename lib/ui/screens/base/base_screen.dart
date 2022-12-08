ab772a8a7b023697fea61549206cb0d2c50246a2
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-12-08 12:41:42 +0330

    Fix bug

--- lib/ui/screens/base/base_screen.dart
+++ lib/ui/screens/base/base_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:pomotimer/data/models/pomodoro_task_model.dart';
 import 'package:pomotimer/routes/routes_name.dart';
 import 'package:pomotimer/ui/screens/base/widgets/custom_bottom_navigation_bar.dart';
 import 'package:pomotimer/ui/screens/calendar/calendar_screen.dart';
@@ -27,6 +28,7 @@ class BaseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: PageView(
         controller: pageController,
         physics: const NeverScrollableScrollPhysics(),
@@ -54,8 +56,10 @@ class BaseScreen extends StatelessWidget {
           color: Colors.white,
           size: 30.r,
         ),
-        onTap: () {
-          Navigator.pushNamed(context, RoutesName.addPomodoroTaskScreen);
+        onTap: () async {
+          final result = await Navigator.pushNamed(context, RoutesName.addPomodoroTaskScreen);
+          if (result == null) return;
+          Get.find<TasksController>().addTask(result as PomodoroTaskModel);
         },
       ),
     );
