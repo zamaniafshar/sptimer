f1ba6887df4ee0065c1d5682ffc8b578fc01d3f7
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-12-31 22:05:40 +0330

    Init task reportage database

--- lib/dependencies/initial_dependencies.dart
+++ lib/dependencies/initial_dependencies.dart
@@ -2,9 +2,11 @@ import 'package:get/get.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:pomotimer/controller/app_settings_controller.dart';
 import 'package:pomotimer/controller/main_controller.dart';
+import 'package:pomotimer/data/databases/tasks_reportage_database.dart';
 
 Future<void> initInitialDependencies() async {
   await Hive.initFlutter();
   await Get.put(AppSettingsController()).init();
+  await Get.put(TasksReportageDatabase()).init();
   await Get.put(MainController()).init();
 }
