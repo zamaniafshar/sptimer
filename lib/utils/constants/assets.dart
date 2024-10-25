a988f00c9fec6371757851d62bbf49d5aad3034e
MohammadAminZamani.afshar <zamani3509@gmail.com>
2024-10-25 14:20:15 +0330

    refactor: constants

---
+++ lib/utils/constants/assets.dart
@@ -0,0 +1,8 @@
+abstract final class Assets {
+  static const kStatusSoundsBasePath = 'assets/audio/status_sounds/';
+  static const tonesBasePath = 'assets/audio/tones/';
+
+  static const workTimeSoundPath = '${kStatusSoundsBasePath}its_time_to_work.mp3';
+  static const shortBreakTimeSoundPath = '${kStatusSoundsBasePath}take_short_break.mp3';
+  static const longBreakSoundPath = '${kStatusSoundsBasePath}take_long_break.mp3';
+}
--- lib/utils/constants/constants.dart
+++ lib/utils/constants/constants.dart
@@ -1,3 +1,55 @@
-export 'pomodoro_sounds_constant.dart';
-export 'date_time_constants.dart';
-export 'assets_path.dart';
+abstract final class Constants {
+  static const pomodoroTaskReportageDB = 'pomodoroTaskReportageDB';
+
+  static const vibrationPattern = [0, 300, 100, 300];
+
+  static const persianDaysOfWeekName = [
+    'ش',
+    'ی',
+    'د',
+    'س',
+    'چ',
+    'پ',
+    'ج',
+  ];
+
+  static const persianMonthsNames = [
+    'فروردین',
+    'اردیبهشت',
+    'خرداد',
+    'تیر',
+    'مرداد',
+    'شهریور',
+    'مهر',
+    'ابان',
+    'آذر',
+    'دی',
+    'بهمن',
+    'اسفند',
+  ];
+
+  static const englishDaysOfWeekName = [
+    'M',
+    'T',
+    'W',
+    'T',
+    'F',
+    'S',
+    'S',
+  ];
+
+  static const englishMonthsNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+}
