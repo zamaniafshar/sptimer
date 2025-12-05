e5de97e9ace2886ca2bce50a3646792e4e923c0c
Amin Zamani Afshar <zamani35099@gmail.com>
2025-12-05 14:35:07 +0330

    ✨ feat(main): integrate flutter bloc for state management
    - add flutter_bloc dependency
    - implement AppBlocObserver for logging bloc transitions and errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sptimer/common/logger/app_bloc_observer.dart';
 import 'package:sptimer/sptimer_app.dart';
 import 'package:sptimer/common/service_locator/service_locator.dart';
 
-import 'data/repositories/tasks_reportage_repository.dart';
-
 void main() async {
+  Bloc.observer = AppBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
 
   await setupServiceLocator();
